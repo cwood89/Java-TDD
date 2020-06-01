9d2f50600e470d7881f6014bf2fe2e12f490dca6
cwood89 <cwood89@mac.com>
2020-06-01 09:56:18 -0400

    ran tests

---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,24 @@
+package FizzBuzz;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+  @Test
+  public void test() {
+
+  }
+
+  @Test
+  public void shouldProcessInput() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    assertEquals("1", fizzBuzz.processNumber(1));
+    assertEquals("2", fizzBuzz.processNumber(2));
+    assertEquals("Fizz", fizzBuzz.processNumber(3));
+    assertEquals("Buzz", fizzBuzz.processNumber(5));
+    assertEquals("Fizz", fizzBuzz.processNumber(6));
+    assertEquals("Buzz", fizzBuzz.processNumber(10));
+    assertEquals("Fizz Buzz", fizzBuzz.processNumber(15));
+  }
+}
